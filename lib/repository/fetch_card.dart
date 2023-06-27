1192a960ce49d442b5cd72c13e24f9d69d50ce90
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-06-27 12:21:50 -0600

    github actions

--- lib/repository/fetch_card.dart
+++ lib/repository/fetch_card.dart
@@ -12,25 +12,25 @@ class CardData {
 class DataService {
 
 
-  // Future<List<CardData>> fetchData() async {
-  //       await dotenv.load(fileName: ".env");
-  //   var supabaseUrl = dotenv.env['SUPABASE_URL']!;
-  //   var supabaseApiKey = dotenv.env['SUPABASE_ANON_KEY']!;
-  //   final response = await http.get(
-  //     Uri.parse('$supabaseUrl/rest/v1/Cards'),
-  //     headers: {'apikey': supabaseApiKey, 'Content-Type': 'application/json'},
-  //   );
-  //
-  //   if (response.statusCode == 200) {
-  //     // Parse the JSON data
-  //     List<dynamic> data = jsonDecode(response.body);
-  //     return data.map((item) => CardData(
-  //       title: item['title'],
-  //       imageUrl: item['image_logo'], // Note that I changed 'logo_image_url' to 'image_logo' as per your previous messages
-  //     )).toList();
-  //   } else {
-  //     // Handle the error
-  //     throw Exception('Failed to load data');
-  //   }
-  // }
+  Future<List<CardData>> fetchData() async {
+        await dotenv.load(fileName: ".env");
+    var supabaseUrl = dotenv.env['SUPABASE_URL']!;
+    var supabaseApiKey = dotenv.env['SUPABASE_ANON_KEY']!;
+    final response = await http.get(
+      Uri.parse('$supabaseUrl/rest/v1/Cards'),
+      headers: {'apikey': supabaseApiKey, 'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      // Parse the JSON data
+      List<dynamic> data = jsonDecode(response.body);
+      return data.map((item) => CardData(
+        title: item['title'],
+        imageUrl: item['image_logo'], // Note that I changed 'logo_image_url' to 'image_logo' as per your previous messages
+      )).toList();
+    } else {
+      // Handle the error
+      throw Exception('Failed to load data');
+    }
+  }
 }
